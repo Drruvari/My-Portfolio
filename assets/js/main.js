4aee2769e9080a07c25b941c11de29679aa55468
cayd33 <devcaydee@gmail.com>
2022-01-24 00:13:01 +0100

    some fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,7 +253,7 @@ async function getRepos(username) {
   const resp = await fetch(APIURL + username + '/repos');
   const respData = await resp.json();
 
-  addReposToCard(respData);
+  // addReposToCard(respData);
 }
 
 function userRepos(user) {
